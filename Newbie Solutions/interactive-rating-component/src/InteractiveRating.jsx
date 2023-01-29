3410ef2dc8040afb97221f335cba0a8fc420bb71
kaangmamat <80741598+kaangmamat@users.noreply.github.com>
2023-01-30 02:39:04 +0800

    Update InteractiveRating.jsx
    Adding some utilities and remove footer file

--- Newbie Solutions/interactive-rating-component/src/InteractiveRating.jsx
+++ Newbie Solutions/interactive-rating-component/src/InteractiveRating.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import icStar from '../src/assets/icon-star.svg'
 import icIlls from '../src/assets/illustration-thank-you.svg'
-import Footer from './Footer'
 
 const InteractiveRating = () => {
     const [colorSub, setColorSub] = useState()
@@ -28,11 +27,11 @@ const InteractiveRating = () => {
     }
 
     return (
-        <div className='bg-veryDarkBlue2 px-6 h-screen flex flex-col items-center justify-center'>
+        <main className='bg-veryDarkBlue2 px-6 min-h-screen flex flex-col items-center justify-center'>
             <div className='px-6 py-8 bg-darkBlue rounded-xl max-w-md'>
                 <div className={`${isShown ? 'hidden' : ''}`}>
                     <div className='bg-onStar w-14 h-14 rounded-full flex flex-col items-center justify-center'>
-                        <img src={icStar} />
+                        <img src={icStar} alt="Star" />
                     </div>
                     <div>
                         <h1 className='font-overpass font-semibold tracking-wide text-2xl text-white2 py-4'>How did we do?</h1>
@@ -53,7 +52,7 @@ const InteractiveRating = () => {
 
                 <div className={`${isShown ? '' : 'hidden'}`}>
                     <div className='flex flex-col items-center justify-center'>
-                        <img src={icIlls} alt="" />
+                        <img src={icIlls} alt="Illustrations" />
                     </div>
                     <div className='flex justify-center items-center py-6'>
                         <p className='bg-onStar text-orange w-52 flex justify-center font-overpass py-2 rounded-3xl'>You selected {colorSub} out of {numList.length}</p>
@@ -63,10 +62,8 @@ const InteractiveRating = () => {
                         <p className='font-overpass text-mediumGray font-normal text-center'>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
                     </div>
                 </div>
-
             </div>
-            <Footer />
-        </div>
+        </main>
     )
 }
 
